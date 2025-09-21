ace266007ea729b595f8627df83ac36810ee2e94
Samuel Olabode <Solabode499@gmail.com>
2025-09-21 12:26:51 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+const path = require('path')
+
+const ejsMate = require('ejs-mate');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended : true}));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+
+
+const ExpressError = require('./utils/expressError');
+
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.all(/(.*)/, (req, res, next) => {
+    next(new ExpressError('Page not found', 404))
+});
+
+app.use((err, req, res, next)=>{
+    const {statusCode = 500} = err;
+    if(!err.message){
+        err.message = 'Something Went Wrong!'
+    }
+    res.status(statusCode).render('error', {err})
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
