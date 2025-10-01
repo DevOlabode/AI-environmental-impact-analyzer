d31bb96983fcc028c2eb14e1ceebf8f8657cf205
Samuel Olabode <Solabode499@gmail.com>
2025-10-01 17:20:34 -0600

    `Refactor password form in user profile: update CSS and JavaScript to match new HTML structure`

--- public/js/user.js
+++ public/js/user.js
@@ -1,9 +1,9 @@
-const form = document.querySelector('.password');
+const form = document.querySelector('#password');
 const button = document.querySelector('.edit-button');
 
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('.password');
-    const button = document.querySelector('.edit-button');
+    // const form = document.querySelector('.password');
+    // const button = document.querySelector('.edit-button');
 
     button.addEventListener('click', () => {
         form.style.display = form.style.display === 'block' ? 'none' : 'block';
