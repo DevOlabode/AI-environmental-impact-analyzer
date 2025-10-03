38461d793a4a548a24bab24d6e15037030ae194d
Samuel Olabode <Solabode499@gmail.com>
2025-10-03 17:02:23 -0600

    `Added validateProduct middleware to routes/form.js and routes/reciept.js`

--- routes/reciept.js
+++ routes/reciept.js
@@ -5,11 +5,12 @@ const recieptController = require('../controllers/reciept');
 const catchAsync = require('../utils/catchAsync');
 
 const {isLoggedIn} = require('../middleware');
+const {validateProduct } = require('../middleware');
 
 const upload = require('../utils/multer'); 
 
 router.get('/get-reciept', isLoggedIn, recieptController.getReciept);
 
-router.post('/analyseReciept',upload.single('reciept'), isLoggedIn, catchAsync(recieptController.analyseReciept));
+router.post('/analyseReciept', upload.single('reciept'), validateProduct , isLoggedIn, catchAsync(recieptController.analyseReciept));
 
 module.exports = router;
